0b7df330f16f73a09a6ceb42e04e909205f181af
João Coelho <admin.henyah@gmail.com>
2017-10-23 16:14:45 +0200

    Cleanup and changes to build.gradle.

--- src/main/java/net/ericsson/emovs/download/EMPDownloadService.java
+++ src/main/java/net/ericsson/emovs/download/EMPDownloadService.java
@@ -1,18 +1,13 @@
 package net.ericsson.emovs.download;
 
-import android.app.IntentService;
-import android.app.Notification;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.ebs.android.utilities.RunnableThread;
 
-import net.ericsson.emovs.utilities.Logging;
 
 /**
  * Created by Joao Coelho on 2017-10-05.
@@ -119,19 +114,19 @@ public class EMPDownloadService extends Service {
     }
 
     private void showNotification() {
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle("DownloadItem")
+//        Notification notification = new NotificationCompat.Builder(this)
+//                .setContentTitle("DownloadItem")
 //                .setTicker(getResources().getString(R.string.app_name))
-                .setContentText("Downloading...")
+//                .setContentText("Downloading...")
 //                .setSmallIcon(R.drawable.my_icon)
 //                .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
 //                .setContentIntent(contentPendingIntent)
-                .setOngoing(true)
+//                .setOngoing(true)
 //                .setDeleteIntent(contentPendingIntent)  // if needed
-                .build();
-
-        // NO_CLEAR makes the notification stay when the user performs a "delete all" command
-        notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
-        startForeground(NOTIFICATION_ID, notification);
+//                .build();
+//
+//        // NO_CLEAR makes the notification stay when the user performs a "delete all" command
+//        notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
+//        startForeground(NOTIFICATION_ID, notification);
     }
 }
